b3ff10c30797e6922c794a43c01bfd3dd747d2f6
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-04-20 13:04:42 -0300

    Simplificado o Ponto para evitar confusão.

--- src/main.py
+++ src/main.py
@@ -9,34 +9,30 @@ running = True
 
 class Point:
     def __init__(self, x, y):
-        self.x = int(x)
-        self.y = int(y)
-
-class FPoint:
-    def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
+#Ponto Cartesiano, vai de −1,0 a 1,0 conforme o canto da tela.
 class CPoint:
     def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+        self.x = float(x) % 1.0
+        self.y = float(y) % 1.0
 
 class Square:
-    def __init__(self, point, size):
+    def __init__(self, point: Point, size):
         self.pos = point
         self.size = float(size)
 
-    def create_rect(self, point):
-        x = 0.0
-        y = 0.0
+    def create_rect(self):
+        left = self.pos.x - self.size / 2
+        top = self.pos.y - self.size / 2
+        return pygame.Rect(left, top, self.size, self.size)
 
+    #Desenha o quadrado na tela.
+    def draw(self, surface: pygame.Surface, color: pygame.Color):
+        pygame.draw.rect(surface, color, self.create_rect())
 
-
-    def draw(self, surface, color):
-        pygame.draw.rect(surface, )
-
-def to_cartesian(raster_point):
+def to_cartesian(raster_point: Point):
     x, y = raster_point
 
     window_x_center, window_y_center = map(lambda val: val / 2, pygame.display.get_window_size())
@@ -45,12 +41,12 @@ def to_cartesian(raster_point):
 
     return cart_x, cart_y
 
-def to_raster(caster_point):
+def to_raster(caster_point: CPoint):
     x, y = caster_point
     window_x_center, window_y_center = map(lambda val: val / 2, pygame.display.get_window_size())
 
-    raster_x = (window_x_center * x)+ window_x_center
-    raster_y = (window_y_center * y)+ window_y_center
+    raster_x = (window_x_center * x) + window_x_center
+    raster_y = (window_y_center * y) + window_y_center
 
     return raster_x, raster_y
 
@@ -60,7 +56,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    quadrado = Square(FPoint(0.0, 0.0), 25.5)
+    quadrado = Square(Point(0.0, 0.0), 25.5)
 
     mouse_pos = to_cartesian(pygame.mouse.get_pos())
 
@@ -71,9 +67,8 @@ while running:
 
     window.fill([distance_color, distance_color, distance_color])
 
-    rect = pygame.Rect(10.0, 10.0, 10.0, 10.0)
-
-    pygame.draw.rect(pygame.display.get_surface(), [123, 123, 189] , rect)
+    cor = pygame.Color(123, 123, 189)
+    quadrado.draw(pygame.display.get_surface(), cor)
 
     pygame.display.flip()
     clock.tick(60)
